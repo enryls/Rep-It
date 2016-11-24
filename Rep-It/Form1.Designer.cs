fdac04d2af9a6018a14ef27ed8a10f87937b9617
enryls <enryls@hotmail.it>
2016-11-24 01:05:30 +0100

    Main Update
    Set the User Interface;
    Set the Initial idea of execution;

--- Rep-It/Form1.Designer.cs
+++ Rep-It/Form1.Designer.cs
@@ -1,19 +1,69 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace Rep_It
 {
     partial class Form1
     {
 
-        //Override ProcessCmdKey method for permit to Exit with Escape key
+        private Button confirm;
+        private TextBox txtBoxNumExec;
+        private Label lblMessageTimer;
+        private Timer timer1;
+        private TextBox txtBoxTimer;
+        private Label lblTimer;
+        private Label lblNumExec;
+        private Label lblInterval;
+        private TextBox txtBoxInterval;
+        private PictureBox pictureBoxLogo;
+        private Panel panel1;
+        private Label lblGithubPage;
+        private TextBox txtBoxKeyStore;
+        private Label lblKeyStore;
+        private Button btnRestoreKey;
+        private PictureBox pictureBoxGLogoLink;
+
+        //Override ProcessCmdKey method
         protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
         {
+            //If the input is not selected select it
+            if (restoreIt)
+            {
+                //If is no special character set the input as text of Textbox4
+                if (keyData != Keys.Enter && keyData != Keys.Space)
+                {
+                    txtBoxKeyStore.Text = keyData.ToString();
+                }
+                //otherwise set the correct string for the input
+                else
+                    checkKey(keyData);
+            }
+            //When the input is selected restoreIt variable is set to false
+            restoreIt = false;
+
+            //Exit with Escape key 
             if (keyData == Keys.Escape)
             {
                 this.Close();
                 return true;
             }
             return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        //Assign the correct value of "special" case to the input for perform repetition
+        private void checkKey(Keys keyData)
+        {
+            switch (keyData)
+            {
+                case Keys.Enter:
+                    txtBoxKeyStore.Text = "{ENTER}";
+                    break;
+                case Keys.Space:
+                    txtBoxKeyStore.Text = " ";
+                    break;
+
+            }
         }
 
         /// <summary>
@@ -42,21 +92,201 @@ namespace Rep_It
         /// </summary>
         private void InitializeComponent()
         {
-            //Define dimensions and properties for Form
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.confirm = new System.Windows.Forms.Button();
+            this.txtBoxNumExec = new System.Windows.Forms.TextBox();
+            this.lblMessageTimer = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.txtBoxTimer = new System.Windows.Forms.TextBox();
+            this.lblTimer = new System.Windows.Forms.Label();
+            this.lblNumExec = new System.Windows.Forms.Label();
+            this.lblInterval = new System.Windows.Forms.Label();
+            this.txtBoxInterval = new System.Windows.Forms.TextBox();
+            this.pictureBoxLogo = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBoxGLogoLink = new System.Windows.Forms.PictureBox();
+            this.lblGithubPage = new System.Windows.Forms.Label();
+            this.txtBoxKeyStore = new System.Windows.Forms.TextBox();
+            this.lblKeyStore = new System.Windows.Forms.Label();
+            this.btnRestoreKey = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGLogoLink)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // confirm
+            // 
+            this.confirm.BackColor = System.Drawing.Color.LightGray;
+            this.confirm.Location = new System.Drawing.Point(150, 300);
+            this.confirm.Name = "confirm";
+            this.confirm.Size = new System.Drawing.Size(185, 65);
+            this.confirm.TabIndex = 4;
+            this.confirm.Text = "Confirm";
+            this.confirm.UseVisualStyleBackColor = false;
+            this.confirm.Click += new System.EventHandler(this.confirm_Click);
+            // 
+            // txtBoxNumExec
+            // 
+            this.txtBoxNumExec.Location = new System.Drawing.Point(280, 120);
+            this.txtBoxNumExec.Name = "txtBoxNumExec";
+            this.txtBoxNumExec.Size = new System.Drawing.Size(185, 23);
+            this.txtBoxNumExec.TabIndex = 1;
+            // 
+            // lblMessageTimer
+            // 
+            this.lblMessageTimer.AutoSize = true;
+            this.lblMessageTimer.Location = new System.Drawing.Point(150, 382);
+            this.lblMessageTimer.Name = "lblMessageTimer";
+            this.lblMessageTimer.Size = new System.Drawing.Size(0, 15);
+            this.lblMessageTimer.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // txtBoxTimer
+            // 
+            this.txtBoxTimer.Location = new System.Drawing.Point(280, 240);
+            this.txtBoxTimer.Name = "txtBoxTimer";
+            this.txtBoxTimer.Size = new System.Drawing.Size(185, 23);
+            this.txtBoxTimer.TabIndex = 2;
+            // 
+            // lblTimer
+            // 
+            this.lblTimer.AutoSize = true;
+            this.lblTimer.Location = new System.Drawing.Point(280, 220);
+            this.lblTimer.Name = "lblTimer";
+            this.lblTimer.Size = new System.Drawing.Size(37, 15);
+            this.lblTimer.TabIndex = 5;
+            this.lblTimer.Text = "Timer";
+            // 
+            // lblNumExec
+            // 
+            this.lblNumExec.AutoSize = true;
+            this.lblNumExec.Location = new System.Drawing.Point(280, 100);
+            this.lblNumExec.Name = "lblNumExec";
+            this.lblNumExec.Size = new System.Drawing.Size(114, 15);
+            this.lblNumExec.TabIndex = 6;
+            this.lblNumExec.Text = "Number of execution";
+            // 
+            // lblInterval
+            // 
+            this.lblInterval.AutoSize = true;
+            this.lblInterval.Location = new System.Drawing.Point(40, 220);
+            this.lblInterval.Name = "lblInterval";
+            this.lblInterval.Size = new System.Drawing.Size(45, 15);
+            this.lblInterval.TabIndex = 7;
+            this.lblInterval.Text = "Interval";
+            // 
+            // txtBoxInterval
+            // 
+            this.txtBoxInterval.Location = new System.Drawing.Point(40, 240);
+            this.txtBoxInterval.Name = "txtBoxInterval";
+            this.txtBoxInterval.Size = new System.Drawing.Size(185, 23);
+            this.txtBoxInterval.TabIndex = 3;
+            // 
+            // pictureBoxLogo
+            // 
+            this.pictureBoxLogo.Location = new System.Drawing.Point(192, -14);
+            this.pictureBoxLogo.Name = "pictureBoxLogo";
+            this.pictureBoxLogo.Size = new System.Drawing.Size(100, 100);
+            this.pictureBoxLogo.TabIndex = 9;
+            this.pictureBoxLogo.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Silver;
+            this.panel1.Controls.Add(this.pictureBoxGLogoLink);
+            this.panel1.Controls.Add(this.lblGithubPage);
+            this.panel1.Location = new System.Drawing.Point(-1, 412);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(504, 52);
+            this.panel1.TabIndex = 10;
+            // 
+            // pictureBoxGLogoLink
+            // 
+            this.pictureBoxGLogoLink.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxGLogoLink.Image")));
+            this.pictureBoxGLogoLink.Location = new System.Drawing.Point(339, 15);
+            this.pictureBoxGLogoLink.Name = "pictureBoxGLogoLink";
+            this.pictureBoxGLogoLink.Size = new System.Drawing.Size(30, 30);
+            this.pictureBoxGLogoLink.TabIndex = 13;
+            this.pictureBoxGLogoLink.TabStop = false;
+            this.pictureBoxGLogoLink.Click += new System.EventHandler(this.pictureBoxGLogoLink_Click);
+            // 
+            // lblGithubPage
+            // 
+            this.lblGithubPage.AutoSize = true;
+            this.lblGithubPage.Location = new System.Drawing.Point(124, 23);
+            this.lblGithubPage.Name = "lblGithubPage";
+            this.lblGithubPage.Size = new System.Drawing.Size(212, 15);
+            this.lblGithubPage.TabIndex = 11;
+            this.lblGithubPage.Text = "Download and Contribute to the Code on";
+            // 
+            // txtBoxKeyStore
+            // 
+            this.txtBoxKeyStore.HideSelection = false;
+            this.txtBoxKeyStore.Location = new System.Drawing.Point(40, 120);
+            this.txtBoxKeyStore.Name = "txtBoxKeyStore";
+            this.txtBoxKeyStore.ReadOnly = true;
+            this.txtBoxKeyStore.Size = new System.Drawing.Size(185, 23);
+            this.txtBoxKeyStore.TabIndex = 0;
+            // 
+            // lblKeyStore
+            // 
+            this.lblKeyStore.AutoSize = true;
+            this.lblKeyStore.Location = new System.Drawing.Point(40, 100);
+            this.lblKeyStore.Name = "lblKeyStore";
+            this.lblKeyStore.Size = new System.Drawing.Size(135, 15);
+            this.lblKeyStore.TabIndex = 11;
+            this.lblKeyStore.Text = "Press the Key for Store It";
+            // 
+            // btnRestoreKey
+            // 
+            this.btnRestoreKey.Location = new System.Drawing.Point(40, 170);
+            this.btnRestoreKey.Name = "btnRestoreKey";
+            this.btnRestoreKey.Size = new System.Drawing.Size(142, 23);
+            this.btnRestoreKey.TabIndex = 12;
+            this.btnRestoreKey.Text = "Click for Restore the Key";
+            this.btnRestoreKey.UseVisualStyleBackColor = true;
+            this.btnRestoreKey.Click += new System.EventHandler(this.btnRestoreKey_Click);
+            // 
+            // Form1
+            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DimGray;
             this.ClientSize = new System.Drawing.Size(502, 461);
-            this.Text = "Rep-It";
+            this.Controls.Add(this.btnRestoreKey);
+            this.Controls.Add(this.lblKeyStore);
+            this.Controls.Add(this.txtBoxKeyStore);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBoxLogo);
+            this.Controls.Add(this.txtBoxInterval);
+            this.Controls.Add(this.lblInterval);
+            this.Controls.Add(this.lblNumExec);
+            this.Controls.Add(this.lblTimer);
+            this.Controls.Add(this.txtBoxTimer);
+            this.Controls.Add(this.lblMessageTimer);
+            this.Controls.Add(this.txtBoxNumExec);
+            this.Controls.Add(this.confirm);
+            this.Font = new System.Drawing.Font("Roboto Condensed", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "REP-IT";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGLogoLink)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
-        }
 
+        }
         #endregion
+
     }
 }
 
--- Rep-It/Form1.cs
+++ Rep-It/Form1.cs
@@ -12,9 +12,79 @@ namespace Rep_It
 {
     public partial class Form1 : Form
     {
+        //Variables used for perform operation
+        int timeLeft = 10;
+        int anInteger;
+        int secondPause = 1;
+        static bool restoreIt = true;
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void confirm_Click(object sender, EventArgs e)
+        {
+            //Variables for input type control.
+            int n, m, c;
+            bool isNumeric = int.TryParse(txtBoxNumExec.Text, out n);
+            bool isNumeric2 = int.TryParse(txtBoxTimer.Text, out m);
+            bool isNumeric3 = int.TryParse(txtBoxInterval.Text, out c);
+
+            if (isNumeric && isNumeric2 && isNumeric3)
+            {
+                //Conversion and Parsing of variables.
+
+                anInteger = Convert.ToInt32(txtBoxNumExec.Text);
+                anInteger = int.Parse(txtBoxNumExec.Text);
+
+                timeLeft = Convert.ToInt32(txtBoxTimer.Text);
+                timeLeft = int.Parse(txtBoxTimer.Text);
+
+                secondPause = Convert.ToInt32(txtBoxInterval.Text);
+                secondPause = int.Parse(txtBoxInterval.Text);
+
+                //Start timer.
+                timer1.Start();
+            }
+        }
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (timeLeft > 0)
+            {
+                // Display the new time left
+                // by updating the Time Left label.
+                this.lblMessageTimer.Text = "Will be executed " + txtBoxKeyStore.Text + " " + anInteger.ToString() + " starting from " + timeLeft + " seconds";
+                timeLeft = timeLeft - 1;
+            }
+            else
+            {
+                // If the user ran out of time, stop the timer 
+                // and execute operation.
+                timer1.Stop();
+                this.lblMessageTimer.Text = " ";
+                timeLeft = 10;
+                for (int i = 0; i < anInteger; i++)
+                {
+                    SendKeys.Send(txtBoxKeyStore.Text);
+                    System.Threading.Thread.Sleep(secondPause * 1000);
+                }
+            }
+
+        }
+
+        private void btnRestoreKey_Click(object sender, EventArgs e)
+        {
+            //Restore the 0 the variable for control the input 
+            restoreIt = true;
+        }
+
+        private void pictureBoxGLogoLink_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/enryls/Rep-It");
         }
     }
 }
